619ba0ec0d78cd55b6d548f7f8ecb0750efee14f
dipanshuhappy <dipanshuhappy@gmail.com>
2025-07-01 11:56:46 +0530

    NFT finilized

---
+++ packages/foundry/contracts/NFT.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract  NFT is ERC721,ERC721Burnable,ERC721URIStorage,ERC721Royalty{
+    uint256 private _nextTokenId;
+
+    constructor(string memory _name,string memory _symbol,address _receiver, uint96 _feeNumerator)
+    ERC721(_name,_symbol) 
+    
+    {
+        _setDefaultRoyalty(_receiver, _feeNumerator);
+
+    }
+    function safeMint(address to, string memory uri,address receiver, uint96 feeNumerator)
+        public
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        _setTokenRoyalty(tokenId, receiver, feeNumerator);
+        return tokenId;
+    }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Royalty, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+     // Override tokenURI to resolve conflict between ERC721 and ERC721URIStorage
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    
+}
